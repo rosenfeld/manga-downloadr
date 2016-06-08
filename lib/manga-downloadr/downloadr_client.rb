31bf50ec75ed7e6b2c8972b53818c922a947f99f
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2016-06-08 12:15:09 -0300

    Use Curb to download resources
    sudo apt-get install libcurl3 libcurl3-gnutls libcurl4-openssl-dev

--- lib/manga-downloadr/downloadr_client.rb
+++ lib/manga-downloadr/downloadr_client.rb
@@ -1,13 +1,17 @@
 require 'digest'
 require 'thread'
+require 'curb'
 
 module MangaDownloadr
   class DownloadrClient
     def self.setup_http_pool(config)
       @@http_clients = SizedQueue.new(config.download_batch_size)
       config.download_batch_size.times do
-        http_client = Net::HTTP.new(config.domain)
-        http_client.keep_alive_timeout = 10
+        #http_client = Net::HTTP.new(config.domain)
+        http_client = Curl::Easy.new
+        http_client.headers['User-Agent'] = USER_AGENT
+        http_client.set(:HTTP_VERSION, Curl::HTTP_2_0)
+        #http_client.keep_alive_timeout = 10
         @@http_clients << http_client
       end
     end
@@ -17,7 +21,7 @@ module MangaDownloadr
     end
 
     def download_only(uri)
-      http_get(uri, { "User-Agent": USER_AGENT }) if @config.download_only
+      http_get(uri) if @config.download_only
     end
 
     def get(uri, &block)
@@ -25,30 +29,46 @@ module MangaDownloadr
       fn = "tmp/#{digest}"
       return block[Nokogiri::HTML(File.read fn)] if !@config.no_cache && File.exists?(fn)
 
-      response = http_get(uri, { "User-Agent": USER_AGENT })
-      case response.code
-      when "301"
-        get response.headers["Location"], &block
-      when "200"
-        File.write fn, response.body
-        parsed = Nokogiri::HTML(response.body)
-        block.call(parsed)
-      else
-        puts "unexpected code: #{response.code} - #{response.body} (#{uri})"
+      http_get(uri) do |response|
+        case response.code
+        #when "301"
+          #get response.headers["Location"], &block
+        when "200"
+          File.write fn, response.body
+          parsed = Nokogiri::HTML(response.body)
+          block.call(parsed)
+        else
+          puts "unexpected code: #{response.code} - #{response.body} (#{uri})"
+        end
       end
-    rescue Net::HTTPGatewayTimeOut, Net::HTTPRequestTimeOut
-      puts 'WARNING: timeout in downloadr_client'
-      # TODO: naive infinite retry, it will loop infinitely if the link really doesn't exist
-      # so should have a way to control the amount of retries per link
-      sleep 1
-      get(uri, &block)
+#    rescue Net::HTTPGatewayTimeOut, Net::HTTPRequestTimeOut
+#      puts 'WARNING: timeout in downloadr_client'
+#      # TODO: naive infinite retry, it will loop infinitely if the link really doesn't exist
+#      # so should have a way to control the amount of retries per link
+#      sleep 1
+#      get(uri, &block)
     end
 
     protected
 
-    def http_get(*args)
+    class Response
+      attr_reader :code, :body
+      def initialize(code, body)
+        @code, @body = code, body
+      end
+    end
+
+    def http_get(uri)
       http_client = @@http_clients.pop
-      http_client.get *args
+      uri = "http://#{@config.domain}#{uri}" if uri.to_s[0] == '/'
+      http_client.url = uri
+      http_client.perform
+      #puts http_client.response_code, http_client.body_str[0..10]
+      yield Response.new(http_client.response_code.to_s, http_client.body_str)
+      #http_client.get uri, { "User-Agent": USER_AGENT }
+    rescue => e
+      puts "could not download #{uri}: #{e.message}"
+      raise e
     ensure
       @@http_clients << http_client
     end
--- lib/manga-downloadr/image_downloader.rb
+++ lib/manga-downloadr/image_downloader.rb
@@ -2,19 +2,20 @@ module MangaDownloadr
   class ImageDownloader < DownloadrClient
     def fetch(image_src, filename)
       File.delete(filename) if File.exists?(filename)
-      response = http_get(image_src, { "User-Agent": USER_AGENT })
-      case response.code
-      when "301"
-        fetch(response.headers["Location"], filename)
-      when "200"
-        File.open(filename, "w") do |f|
-          f.print response.body
+      http_get(image_src) do |response|
+        case response.code
+        #when "301"
+          #fetch(response.headers["Location"], filename)
+        when "200"
+          File.open(filename, "w") do |f|
+            f.print response.body
+          end
         end
       end
-    rescue Net::HTTPGatewayTimeOut, Net::HTTPRequestTimeOut
-      puts 'WARNING: timeout in image_downloader'
-      sleep 1
-      fetch(image_src, filename)
+#    rescue Net::HTTPGatewayTimeOut, Net::HTTPRequestTimeOut
+#      puts 'WARNING: timeout in image_downloader'
+#      sleep 1
+#      fetch(image_src, filename)
     end
   end
 end
